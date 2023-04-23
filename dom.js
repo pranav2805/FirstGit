dff8ca1ce2c4531efb766eede61e53e195e07092
pranav2805 <pranavpradeep095@gmail.com>
2023-04-23 18:43:58 +0530

    Task2 dom js

---
+++ dom.js
@@ -0,0 +1,13 @@
+//console.dir(document);
+
+const title = document.getElementById('header-title');
+//console.log(title);
+
+title.style.color = 'blue';
+
+const mainHeader = document.getElementById('main-header');
+mainHeader.style.borderBottom = 'solid 3px #000';
+
+const addItems = document.getElementById('add-items');
+console.log(addItems);
+addItems.style.color = 'green';
